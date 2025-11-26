c12c5489549abfa00174eba708c379ea7d571831
Neelsavani94 <99803440+Neelsavani94@users.noreply.github.com>
2025-11-26 11:48:29 +0530

    New Project Setup

---
+++ android/app/src/main/kotlin/com/vekariya_brothers_manager/app/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.vekariya_brothers_manager.app
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterFragmentActivity() {
+}
---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/hardikshiroya/Documents/flutter_latest"
+export "FLUTTER_APPLICATION_PATH=/Users/hardikshiroya/Downloads/vekariya_brothers_manager"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/hardikshiroya/Downloads/vekariya_brothers_manager/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/hardikshiroya/Downloads/vekariya_brothers_manager/.dart_tool/package_config.json"
